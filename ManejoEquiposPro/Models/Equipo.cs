72c5c3a09ced10a9edea4794a89e8e9ed8778341
Leonardo <leonardoandrade2003.197@gmail.com>
2024-10-18 08:59:37 -0500

    Cree clases Equipo y jugador en models

---
+++ ManejoEquiposPro/Models/Equipo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ManejoEquiposPro.Models
+{
+    public class Equipo
+    {
+        [Key]
+        public int IdEquipo { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Ciudad { get; set; }
+
+        [Required]
+
+        public int titulos { get; set; }
+
+        [Required]
+        public Boolean AceptaExtranjero { get; set; }
+
+         
+    }
+}
---
+++ ManejoEquiposPro/Models/Jugador.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ManejoEquiposPro.Models
+{
+    public class Jugador
+    {
+
+        [Key]
+        public int IdJugador { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Posicion { get; set; }
+        [Required]
+        public int Edad {  get; set; }
+
+        public Equipo Equipo { get; set; }
+
+        [ForeignKey("Equipo")]
+        public int IdEquipo { get; set; }
+    }
+}
